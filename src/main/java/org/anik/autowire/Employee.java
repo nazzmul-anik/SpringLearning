28d0634e4d7e2d66395befb4c0e4d3c2d922f169
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2025-01-05 08:25:02 +0600

    Autowiring using XML

---
+++ src/main/java/org/anik/autowire/Employee.java
@@ -0,0 +1,28 @@
+package org.anik.autowire;
+
+public class Employee {
+    private Address address;
+
+    public Employee() {
+    }
+
+    public Employee(Address address) {
+        this.address = address;
+        System.out.println("Inside Employee Constructor");
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "address=" + address +
+                '}';
+    }
+}
---
+++ src/main/java/org/anik/autowire/Test.java
@@ -0,0 +1,12 @@
+package org.anik.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("autoconfig.xml");
+        Employee employee = (Employee) context.getBean("employee");
+        System.out.println(employee);
+    }
+}
