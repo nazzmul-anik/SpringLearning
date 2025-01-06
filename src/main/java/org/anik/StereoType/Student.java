6fb93c904ebd9e7336b1b6ce59a4f9a3c440e896
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2025-01-06 07:13:55 +0600

    Here, I write code about how to add util objects using @Component and @Value

--- src/main/java/org/anik/StereoType/Student.java
+++ src/main/java/org/anik/StereoType/Student.java
@@ -3,12 +3,16 @@ package org.anik.StereoType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component("ob")
 public class Student {
     @Value("Anik")
     private String studentName;
     @Value("Dhaka")
     private String city;
+    @Value("#{addressList}")
+    private List<String> address;
 
     public Student() {
     }
@@ -34,11 +38,20 @@ public class Student {
         this.city = city;
     }
 
+    public List<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<String> address) {
+        this.address = address;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "studentName='" + studentName + '\'' +
                 ", city='" + city + '\'' +
+                ", address=" + address +
                 '}';
     }
 }
--- src/main/java/org/anik/StereoType/Test.java
+++ src/main/java/org/anik/StereoType/Test.java
@@ -7,6 +7,7 @@ public class Test {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("stereoconfig.xml");
         Student student = (Student) context.getBean("ob");
-        System.out.println(student);
+        System.out.println(student.getAddress());
+        System.out.println(student.getAddress().getClass());
     }
 }
