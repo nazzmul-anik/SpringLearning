a313a6f1e712bca45b7c9a290654afde73f2c200
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2025-01-06 08:50:40 +0600

    Using Spring Expression Language add static method, variable and object.

--- src/main/java/org/anik/springexpressionlanguage/Demo.java
+++ src/main/java/org/anik/springexpressionlanguage/Demo.java
@@ -9,6 +9,14 @@ public class Demo {
     private int x;
     @Value("#{22/2}")
     private int y;
+    @Value("#{T(java.lang.Math).sqrt(25)}")
+    private int z;
+    @Value("#{T(java.lang.Math).E}")
+    private double e;
+    @Value("#{T(java.lang.Math).PI}")
+    private double pi;
+    @Value("#{ new java.lang.String('Md. Nazmul Hasan Anik')}")
+    private String name;
 
     public int getX() {
         return x;
@@ -26,11 +34,47 @@ public class Demo {
         this.y = y;
     }
 
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public double getE() {
+        return e;
+    }
+
+    public void setE(double e) {
+        this.e = e;
+    }
+
+    public double getPi() {
+        return pi;
+    }
+
+    public void setPi(double pi) {
+        this.pi = pi;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "Demo{" +
                 "x=" + x +
                 ", y=" + y +
+                ", z=" + z +
+                ", e=" + e +
+                ", pi=" + pi +
+                ", name='" + name + '\'' +
                 '}';
     }
 }
